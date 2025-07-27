feeb69bc17d121886219c6006999493902223cc0
naymurdev <naymurrahman178@gmail.com>
2025-07-27 23:31:06 +0600

    fix(blocks): resolved blocks page

--- apps/ui-layout/lib/code.ts
+++ apps/ui-layout/lib/code.ts
@@ -1,7 +1,33 @@
 import fs from 'fs';
 import path from 'path';
+const REPO_ROOT = path.resolve(process.cwd(), '../..')
 
 export const extractCodeFromFilePath = (filePath: string) => {
-  const absolutePath = path.join(process.cwd(), filePath);
-  return fs.readFileSync(absolutePath, 'utf-8');
+  try {
+    console.log("filePath",filePath);
+    
+    const absolutePath = path.join(process.cwd(), filePath);
+    return fs.readFileSync(absolutePath, 'utf-8');
+  } catch (e) {
+    console.error("Failed to load code file:", filePath, e);
+    return "// Code not found";
+  }
+};
+
+
+export const extractCodeFromPackages = (filePaths: string[] | string) => {
+  const paths = Array.isArray(filePaths) ? filePaths : [filePaths];
+console.log(paths);
+
+  return paths.map((filePath) => {
+    try {
+      const absolutePath = path.join(REPO_ROOT, filePath);
+      const code = fs.readFileSync(absolutePath, 'utf-8');
+      const name = path.basename(filePath);
+      return { id: name, name, code };
+    } catch (e) {
+      console.error("Failed to load code file:", filePath, e);
+      return { id: filePath, name: filePath, code: "// Code not found" };
+    }
+  });
 };
