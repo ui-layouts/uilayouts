4cefa16e8c210140388a1c3b6a3f5cafffe8d80a
Yeom-JinHo <pwlsghq@naver.com>
2025-09-19 23:30:58 +0900

    feat: remove unused type

--- apps/ui-layout/types/docs.ts
+++ apps/ui-layout/types/docs.ts
@@ -5,25 +5,6 @@ export interface DocsFileRef {
   filesrc: string;
 }
 
-export interface DocsComponentItem {
-  componentName: ComponentKey;
-  filesrc?: string;
-  iframeSrc?: string;
-  iframelink?: string;
-  iframeLink?: string;
-  filesArray?: DocsFileRef[];
-}
-
-export interface DocsSection {
-  name: DocsCategory;
-  componentArray: DocsComponentItem[];
-  type: DocsCategory;
-}
-
-export interface DocsRoot {
-  dataArray: DocsSection[];
-}
-
 export type DocsCategory = (typeof DOCS_CATEGORY)[keyof typeof DOCS_CATEGORY];
 
 export type ComponentKey = (typeof COMPONENT_KEYS)[keyof typeof COMPONENT_KEYS];
