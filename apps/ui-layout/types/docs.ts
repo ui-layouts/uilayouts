42999b0d7a3b5f754bf21431ce3465f5cf615e07
Yeom-JinHo <pwlsghq@naver.com>
2025-09-19 21:08:53 +0900

    feat: add COMPONENT_KEYS constants for type-safe component names

--- apps/ui-layout/types/docs.ts
+++ apps/ui-layout/types/docs.ts
@@ -1,4 +1,4 @@
-import { DOCS_CATEGORY } from '@/const/docs';
+import { COMPONENT_KEYS, DOCS_CATEGORY } from '@/const/docs';
 
 export interface DocsFileRef {
   name: string;
@@ -25,3 +25,5 @@ export interface DocsRoot {
 }
 
 export type DocsCategory = (typeof DOCS_CATEGORY)[keyof typeof DOCS_CATEGORY];
+
+export type ComponentKey = (typeof COMPONENT_KEYS)[keyof typeof COMPONENT_KEYS];
